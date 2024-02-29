45b0fa97e4aa8c47ec972e61637e41607e88d952
harikishanmatrix <harikishan14087@gmail.com>
2024-02-29 16:39:22 +0530

    todochanges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import Modal from "./Modal";
+let App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [inputValues, setInputValues] = useState([]);
+  const [isOpen, setIsopen] = useState(false);
+
+  const closeModal = () => {
+    setIsopen(false);
+  };
+
+  const tasksIn = () => {
+    setIsopen(true);
+  };
+
+  const handleSubmit = (value) => {
+    setInputValue(value);
+    setInputValues([...inputValues, inputValue]);
+  };
+
+  let [btn, setBtn] = useState("+");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border-2 border-black">
+      <div>
+        <div className="h-full m-2 p-2 flex justify-between">
+          <h1 className="font-bold">Tasks</h1>
+          <button className="font-bold" onClick={tasksIn}>
+            {btn}
+          </button>
+        </div>
+      </div>
+      <div>
+        {inputValues.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </div>
+
+      <div>
+        {<Modal open={isOpen} onClose={closeModal} submit={handleSubmit} />}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
