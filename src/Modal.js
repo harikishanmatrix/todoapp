42ac188a92d221c96bba2dfc9d2ed34b9aed1f64
harikishanmatrix <harikishan14087@gmail.com>
2024-03-04 18:17:25 +0530

    Final Accordian

--- src/Modal.js
+++ src/Modal.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 
-const Modal = ({ value, setValue, array, index }) => {
+const Modal = ({ value, setValue, array, indexes, index }) => {
   const [arr, setArr] = useState([array]);
-  const [active, isActive] = useState(0);
+
   const handleClick = (index) => {
     console.log(index);
+    setValue(index);
   };
 
   return (
     <div>
       <div>
-        <div>
-          <h1 key={index}>{arr}</h1>
-        </div>
-
-        <button onClick={() => handleClick(index)}>{arr}</button>
+        <button
+          onClick={() => handleClick(index)}
+          className="bg-black text-white"
+        >
+          {arr}
+        </button>
+        {indexes && <h1 key={index}>{arr}</h1>}
       </div>
     </div>
   );
